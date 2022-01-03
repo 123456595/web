a8addcadbc4e65d4d5ae4c19e1007e758e274da7
123456595 <82946316+123456595@users.noreply.github.com>
2022-01-03 16:12:43 +0800

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,133 @@
+# web
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>豆豆的博客</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+/* 设置页眉样式 */
+.header {
+  background-color: #f1f1f1;
+  padding: 20px;
+  text-align: center;
+}
+
+/* 设置顶部导航栏的样式 */
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+/* 设置 topnav 链接的样式 */
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* 改变鼠标悬停时的颜色 */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* 创建并排的三个非等列 */
+.column {
+  float: left;
+  padding: 10px;
+}
+
+/* 左和右列 */
+.column.side {
+  width: 25%;
+}
+
+/* 中间列 */
+.column.middle {
+  width: 50%;
+}
+
+/* 清除列之后的浮动 */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+  color:red ;
+}
+
+/* 响应式布局 - 创建堆叠而非并排的三列 */
+@media screen and (max-width: 600px) {
+  .column.side, .column.middle {
+    width: 100%;
+  }
+}
+
+/* 设置页脚的样式 */
+.footer {
+  background-color: green;
+  padding: 10px;
+  text-align: center;
+}
+body {
+  background-color: lightblue;
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>豆豆的博客</h1>
+  <p>Welcome to my blog!</p>
+</div>
+
+<div class="topnav">
+  <a href="http://119.91.83.246/ht2.php">首页</a>
+  <a href="http://119.91.83.246/ht3.php">信息服务</a>
+  <a href="http://119.91.83.246/ht5.php">API接口</a>
+</div>
+
+<div class="row">
+  <div class="column side">
+    <h2>创建时间</h2>
+    <p>豆豆的博客已经勉强存活了
+    <?php 
+    $time = time();
+    $time = $time-1636379116;
+    echo $time;
+    ?>
+    秒</p>
+  </div>
+  
+  <div class="column middle">
+    <h2>主要介绍：</h2>
+    <p>一个初一的小伙子，会一点点编程技术......</p>
+    <p>咳咳，好了，不吹牛了，其实我只会一点点pytho
+    n,和HTML、CSS。呜呜呜。哎，大家可以点击此页面上方的导航栏，点击“API接口”，就可以跳转到我的API接口了！</p>
+    <p>欢迎大家的访问哦！你们的访问量才是我制作接口的动力！</p>
+    <p>加油！</p>
+  </div>
+  
+  <div class="column side">
+    <h2>Side</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
+  </div>
+</div>
+
+<div class="footer">
+  <p>欢迎访问</p>
+</div>
+  
+</body>
+</html>
